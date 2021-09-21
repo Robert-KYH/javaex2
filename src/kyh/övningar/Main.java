3492bc3e2f3226707a1236f29a72e6fb0ffc6389
Robert <robert.leffmann@student.kyh.se>
2021-09-21 14:34:30 +0200

    Nytt projekt

---
+++ src/kyh/övningar/Main.java
@@ -0,0 +1,15 @@
+//  https://www.w3resource.com/java-exercises/search/index.php
+
+package kyh.övningar;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+  private static Scanner in;
+
+  public static void main(String[] args) {
+    in = new Scanner(System.in);
+  }
+}
